8f303dad1d50ba9e69cb9f4e8fb8d9756f31e8d3
thebalthazar <balthazar@Balthas-MacBook-Pro.local>
2014-06-22 15:50:01 -0300

    newcontact

---
+++ Site/Contato_files/Contato.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20140622
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contato_files/ContatoMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');performPostEffectsFixups()}
